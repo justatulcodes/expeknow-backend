be189be91e47c410967beadbf74c9519bc0a998e
Atul Kumar <atul@byte-digital.com.au>
2025-11-18 23:27:21 +0530

    added initial git repo for expeknow backend

---
+++ src/main/kotlin/com/expeknow/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package com.expeknow.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
